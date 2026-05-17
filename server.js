fa888efaa6d269eae18384dda8953921ba806b54
benssy <benssyjk@gmail.com>
2026-05-17 19:58:52 +0200

    changed code for azure deploy

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2026 DevOps Course von kolatben!');
+  res.send('Hello FS2026 DevOps Course von kolatben!Deployed auf azure via github actions');
 });
 
 app.listen(PORT, HOST);
