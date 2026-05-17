b6ee535553397cc1a5c7fc2d1f45fcd5a0466ed3
benssy <benssyjk@gmail.com>
2026-05-17 19:44:37 +0200

    back to black

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2026 DevOps Course von kolatben!Deployed auf azure via github test!');
+  res.send('Hello FS2026 DevOps Course von kolatben!');
 });
 
 app.listen(PORT, HOST);
