d2a799d1ecceb5d87bd95bfce0a1c553fe9e06ec
benssy <benssyjk@gmail.com>
2026-05-17 19:34:46 +0200

    test

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2026 DevOps Course von kolatben!Deployed auf Render!');
+  res.send('Hello FS2026 DevOps Course von kolatben!Deployed auf azure via github test!');
 });
 
 app.listen(PORT, HOST);
